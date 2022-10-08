3ca3c6fd2c36b9b626fcce5b4e2ebc03b9fd1486
Ian Markowich <ian@maxus33.com>
2022-10-08 15:46:41 -0400

    Fixes R49 Compliance

--- R49_MSC06_j.java
+++ R49_MSC06_j.java
@@ -1,16 +1,6 @@
 
-class BadIterate {
-  public static void main(String[] args) {
-    List<String> list = new ArrayList<String>();
-    list.add("one");
-    list.add("two");
-
-    Iterator iter = list.iterator();
-    while (iter.hasNext()) {
-      String s = (String)iter.next();
-      if (s.equals("one")) {
-        list.remove(s);
-      }
-    }
-  }
+// ...
+if (s.equals("one")) {
+  iter.remove();
 }
+// ...
