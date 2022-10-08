dc72fa6225cfc8e69b2c8fde39e67dd5ee8cc186
Ian Markowich <ian@maxus33.com>
2022-10-08 15:40:15 -0400

    Adds noncompliant R13

---
+++ R13_FI009_j.java
@@ -0,0 +1,7 @@
+class ConsoleWrite {
+  public static void main(String[] args) {
+    // Any input value > 255 will result in unexpected output
+    System.out.write(Integer.valueOf(args[0]));
+    System.out.flush();
+  }
+}
